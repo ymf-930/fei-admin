404762941fb04bd4281ddef35849a906248b38b0
ymf <1950714773@qq.com>
2023-03-07 00:06:39 +0800

    样式文件引入

--- src/main.js
+++ src/main.js
@@ -7,12 +7,9 @@ import { registerUI } from '@/plugins/f-ui'
 import { registerCharts } from '@/plugins/bin-charts-next'
 import { registerEditor } from '@/plugins/bin-editor-next'
 import { registerCommonComps } from '@/components/Common/index'
-// import 'f-ui-one/lib/styles/normalize.css' // 初始化样式
-// import 'f-ui-one/lib/styles/scrollbar.css' // 滚动条样式
-// import 'f-ui-one/lib/styles/index.css' // 组件库样式
-import 'f-ui-one/styles/normalize.css' // 初始化样式
-import 'f-ui-one/styles/scrollbar.css' // 滚动条样式
-import 'f-ui-one/styles/index.css' // 组件库样式
+import 'f-ui-one/lib/styles/normalize.css' // 初始化样式
+import 'f-ui-one/lib/styles/scrollbar.css' // 滚动条样式
+import 'f-ui-one/lib/styles/index.css' // 组件库样式
 import '@/assets/stylus/index.styl' // 项目样式
 
 /**
