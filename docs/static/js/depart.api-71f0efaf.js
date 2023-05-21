04aa33222c4782da66e4756d18ece8c480f1614c
ymf <1950714773@qq.com>
2023-05-21 17:14:19 +0800

    项目依赖更新并重新打包部署

--- docs/static/js/depart.api-71f0efaf.js
+++
@@ -1,1 +0,0 @@
-import{i as e}from"./index-13697b49.js";function r(){return e({url:"/depart/tree",method:"get"})}export{r as g};
--- docs/static/js/user.api-c5a9880b.js
+++
@@ -1,1 +0,0 @@
-import{i as t}from"./index-13697b49.js";function r(){return t({url:"/user/list",method:"get"})}export{r as g};
