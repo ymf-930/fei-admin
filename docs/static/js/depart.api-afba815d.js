19f131d77381aaca4fa12afa21e0023928cd0c96
ymf <1950714773@qq.com>
2023-09-02 21:03:42 +0800

    项目打包部署

---
+++ docs/static/js/depart.api-afba815d.js
@@ -0,0 +1,1 @@
+import{i as e}from"./index-e44e98cb.js";function r(){return e({url:"/depart/tree",method:"get"})}export{r as g};
---
+++ docs/static/js/user.api-162dd5cd.js
@@ -0,0 +1,1 @@
+import{i as t}from"./index-e44e98cb.js";function r(){return t({url:"/user/list",method:"get"})}export{r as g};
